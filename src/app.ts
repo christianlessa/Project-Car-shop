e99ed288f5c5af3ee2d8586407cdffc47a96cc97
Christian Lessa <christianclessa@gmail.com>
2022-08-29 17:28:54 -0300

    Adicionando no app a rota motorcycles

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import 'express-async-errors';
 import express from 'express';
 import carRouter from './routes/Car';
+import motorcycleRouter from './routes/Motorcycle';
 import errorHandler from './middlewares/error';
 
 const app = express();
 
 app.use(express.json());
 app.use(carRouter);
+app.use(motorcycleRouter);
 app.use(errorHandler);
 
 export default app;
