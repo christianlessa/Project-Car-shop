1d8044a5f361759d377668a2e2f4bbd8d793ed6d
Christian Lessa <christianclessa@gmail.com>
2022-08-29 17:28:03 -0300

    Criando Schema motorcycle

--- src/schemas/motorcycle.ts
+++ src/schemas/motorcycle.ts
@@ -3,7 +3,7 @@ import vehicleZodSchema from './vehicle';
 
 const motorcycleZodSchema = vehicleZodSchema.extend({
   category: z.enum(['Street', 'Custom', 'Trail']),
-  engineCapacity: z.number().gte(0).lte(2500).int(),
+  engineCapacity: z.number().gte(1).lte(2500).int(),
 });
 
 export default motorcycleZodSchema;
