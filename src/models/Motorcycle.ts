530ecb261c512b2685755d28e0ab69897b0dc72d
Christian Lessa <christianclessa@gmail.com>
2022-08-29 17:25:37 -0300

    Criando Model Motorcycles

---
+++ src/models/Motorcycle.ts
@@ -0,0 +1,21 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { IMotorcycle } from '../interfaces/IMotorcycle';
+import MongoModel from './MongoModel';
+
+const motorcycleSchema = new Schema<IMotorcycle>({
+  model: String,
+  year: Number,
+  color: String,
+  status: Boolean,
+  buyValue: Number,
+  category: String,
+  engineCapacity: Number,
+}, { versionKey: false });
+
+class MotorcycleModel extends MongoModel<IMotorcycle> {
+  constructor(model = mongooseCreateModel('Motorcycle', motorcycleSchema)) {
+    super(model);
+  }
+}
+
+export default MotorcycleModel; 
